8422a64bc314304157e1a08efd431ebddf17cc6d
Sauva3k <sauva3k@hotmail.com>
2017-04-02 00:56:10 +1100

    Add initial user page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root 'home_page#home'
   get 'home_page/home'
   get '/signup', to: 'users#new'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
