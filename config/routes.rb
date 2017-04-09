1ad08d2e64e1c76f35e591c9346997f428e7e5ca
Sauva3k <sauva3k@hotmail.com>
2017-04-09 13:59:29 +1000

    Mark item as complete function

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,11 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do
+	  member do
+	    patch :complete
+	  end
+    end
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
