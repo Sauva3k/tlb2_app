13d34a2c81661fd789a2f525d4a227142a3e273f
Sauva3k <sauva3k@hotmail.com>
2017-04-12 21:23:57 +1000

    Add task_items controller

--- app/models/task_item.rb
+++ app/models/task_item.rb
@@ -1,4 +1,7 @@
 class TaskItem < ApplicationRecord
   belongs_to :user
-
+  default_scope -> { order(created_at: :desc) }
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates :points, presence: true
 end
